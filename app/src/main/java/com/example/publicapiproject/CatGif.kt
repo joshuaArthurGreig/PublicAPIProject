df92e4ca2ec9bddef12ac966991d090fa57a8528
joshuaArthurGreig <jagreig15@gmail.com>
2022-03-01 10:06:22 -0800

    first commit

---
+++ app/src/main/java/com/example/publicapiproject/CatGif.kt
@@ -0,0 +1,9 @@
+package com.example.publicapiproject
+
+import retrofit2.Call
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface CatGif {
+    @GET("")
+}
---
+++ app/src/main/java/com/example/publicapiproject/RetrofitHelper.kt
@@ -0,0 +1,6 @@
+package com.example.publicapiproject
+
+object RetrofitHelper {
+
+    val baseURL = "cataas.com/"
+}
